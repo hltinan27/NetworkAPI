be3b7a497926bd601e319e7de73ba9867a0e7926
Halit inan <halit.inan@aktifbank.com.tr>
2020-02-11 09:36:26 +0300

    Initial Commit

---
+++ NetworkAPI/NetworkAPI.h
@@ -0,0 +1,19 @@
+//
+//  NetworkAPI.h
+//  NetworkAPI
+//
+//  Created by Halit inan on 11.02.2020.
+//  Copyright © 2020 Halit inan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkAPI.
+FOUNDATION_EXPORT double NetworkAPIVersionNumber;
+
+//! Project version string for NetworkAPI.
+FOUNDATION_EXPORT const unsigned char NetworkAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkAPI/PublicHeader.h>
+
+
